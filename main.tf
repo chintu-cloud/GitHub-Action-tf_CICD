de4230127d8ffceb7be005690125ef9a97c87135
chintu-cloud <github.chintu02@gmail.com>
2025-11-24 13:20:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,17 @@
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
-      Name = "cloud-vpc"
+      Name = "cloud_vpc"
     }
+
 }
 
-resource "aws_subnet" "name" {
+
+resource "aws_subnet" "name2" {
     vpc_id = aws_vpc.name.id
     cidr_block = "10.0.0.0/24"
     tags = {
-        Name = "subnet-cloud"
-    } 
+      Name = "cloud-subnet"
+    }
+  
 }
